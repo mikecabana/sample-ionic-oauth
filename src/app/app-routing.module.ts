ff1da8cf59d4e8b6af27b44cd2d807b6ae750985
mikecabana <mikeyc343@gmail.com>
2021-03-22 12:59:52 -0400

    removed slides root page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,46 +3,41 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthGuardService } from './core/auth-guard.service';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'landing',
-    pathMatch: 'full',
-  },
-  {
-    path: 'tabs',
-    canActivate: [AuthGuardService],
-    loadChildren: () =>
-      import('./tabs/tabs.module').then((m) => m.TabsPageModule),
-  },
-  {
-    path: 'auth/callback',
-    loadChildren: () =>
-      import('./auth/auth-callback/auth-callback.module').then(
-        (m) => m.AuthCallbackPageModule
-      ),
-  },
-  {
-    path: 'auth/endsession',
-    loadChildren: () =>
-      import('./auth/end-session/end-session.module').then(
-        (m) => m.EndSessionPageModule
-      ),
-  },
-  {
-    path: 'landing',
-    loadChildren: () =>
-      import('./landing/landing.module').then((m) => m.LandingPageModule),
-  },
-  {
-    path: 'root',
-    loadChildren: () =>
-      import('./root/root.module').then((m) => m.RootPageModule),
-  },
+    {
+        path: '',
+        redirectTo: 'landing',
+        pathMatch: 'full',
+    },
+    {
+        path: 'tabs',
+        canActivate: [AuthGuardService],
+        loadChildren: () =>
+            import('./tabs/tabs.module').then((m) => m.TabsPageModule),
+    },
+    {
+        path: 'auth/callback',
+        loadChildren: () =>
+            import('./auth/auth-callback/auth-callback.module').then(
+                (m) => m.AuthCallbackPageModule
+            ),
+    },
+    {
+        path: 'auth/endsession',
+        loadChildren: () =>
+            import('./auth/end-session/end-session.module').then(
+                (m) => m.EndSessionPageModule
+            ),
+    },
+    {
+        path: 'landing',
+        loadChildren: () =>
+            import('./landing/landing.module').then((m) => m.LandingPageModule),
+    },
 ];
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
-  ],
-  exports: [RouterModule],
+    imports: [
+        RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+    ],
+    exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
